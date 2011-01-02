0074d1e933b6287b8dc295de42221f0979a8f0c2
Gora Khargosh <gora.khargosh@gmail.com>
2011-01-02 14:27:10 +0530

    ADds a match_any_paths function
    Signed-off-by: Gora Khargosh <gora.khargosh@gmail.com>

--- pathtools/patterns.py
+++ pathtools/patterns.py
@@ -36,7 +36,10 @@ Functions
 
 from fnmatch import fnmatch, fnmatchcase
 
-__all__ = ['match_path', 'match_path_against', 'filter_paths']
+__all__ = ['match_path',
+           'match_path_against',
+           'match_any_paths',
+           'filter_paths']
 
 
 def _string_lower(s):
@@ -213,3 +216,50 @@ def filter_paths(pathnames,
         # above.
         if _match_path(pathname, included, excluded, case_sensitive):
             yield pathname
+
+def match_any_paths(pathnames,
+                    included_patterns=None,
+                    excluded_patterns=None,
+                    case_sensitive=True):
+    """
+    Matches from a set of paths based on acceptable patterns and
+    ignorable patterns.
+
+    :param pathnames:
+        A list of path names that will be filtered based on matching and
+        ignored patterns.
+    :param included_patterns:
+        Allow filenames matching wildcard patterns specified in this list.
+        If no pattern list is specified, ["*"] is used as the default pattern,
+        which matches all files.
+    :param excluded_patterns:
+        Ignores filenames matching wildcard patterns specified in this list.
+        If no pattern list is specified, no files are ignored.
+    :param case_sensitive:
+        ``True`` if matching should be case-sensitive; ``False`` otherwise.
+    :returns:
+        ``True`` if any of the paths matches; ``False`` otherwise.
+
+    Doctests::
+        >>> pathnames = set(["/users/gorakhargosh/foobar.py", "/var/cache/pdnsd.status", "/etc/pdnsd.conf", "/usr/local/bin/python"])
+        >>> match_any_paths(pathnames)
+        True
+        >>> match_any_paths(pathnames, case_sensitive=False)
+        True
+        >>> match_any_paths(pathnames, ["*.py", "*.conf"], ["*.status"], case_sensitive=True)
+        True
+        >>> match_any_paths(pathnames, ["*.txt"], case_sensitive=False)
+        False
+        >>> match_any_paths(pathnames, ["*.txt"], case_sensitive=True)
+        False
+    """
+    included = ["*"] if included_patterns is None else included_patterns
+    excluded = [] if excluded_patterns is None else excluded_patterns
+
+    for pathname in pathnames:
+        # We don't call the public match_path because it checks arguments
+        # and sets default values if none are found. We're already doing that
+        # above.
+        if _match_path(pathname, included, excluded, case_sensitive):
+            return True
+    return False
